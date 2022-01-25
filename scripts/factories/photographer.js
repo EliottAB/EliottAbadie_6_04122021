632533c190634592b672ac6b75101048b6474646
Abadie Eliott <eliott.abadiesouchar@icloud.com>
2022-01-25 09:18:46 +0100

    tags header, medias added, like and sort function

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,6 +1,7 @@
 //Prends les datas dans le fichier JSON
 let data = []
 let criteres = []
+let headtaged = false
 fetch("/data/photographers.json")
 .then((res) => {
     const promesse = res.json();
@@ -8,6 +9,7 @@ fetch("/data/photographers.json")
 })
 .then((datas) =>{
     data = datas
+    alltags = (datas.AllTags[0].tags)
     init()
 })
 
@@ -23,103 +25,111 @@ async function getPhotographers() {
         const picture = `assets/photographers/${portrait}`;
         
         function getUserCardDOM(page) {
-        const article = document.createElement( 'article' );
-        const section = document.createElement( 'section' );
-        const img = document.createElement( 'img' );
-        const h2 = document.createElement( 'h2' );
-        const plocation = document.createElement( 'p' );
-        const ptagline = document.createElement( 'p' );
-        const pprice = document.createElement( 'p' );
-        const a = document.createElement( 'a' )
-        const headercriteres = document.getElementById("headercriteres")
-
-        img.setAttribute("src", picture)
-        img.setAttribute("alt", "Portrait de " + name)
-        h2.textContent = name;
-        plocation.textContent = city + ", " + country;
-        ptagline.textContent = tagline;
-        pprice.textContent = price + "€/jour";
-
-        plocation.classList.add("location")
-        ptagline.classList.add("tagline")
-        pprice.classList.add("price")
-        
-        if(page == "home"){
-            article.appendChild(a);
-            a.setAttribute("href", "./photographer.html?id="+ id);
-            a.appendChild(img);
-        }
-        
-        article.appendChild(h2);
-        article.appendChild(plocation);
-        article.appendChild(ptagline);
-        
-        if(page == "home"){
-            article.appendChild(pprice);
-            section.classList.add("categories")
-            article.appendChild(section);
-            hashtags()
-        }
-
-        //Ajoute les hashtags et leurs utilitées
-        function hashtags(){
-            for(let x = 0; x != categories.length; x++){
-                let button = section.appendChild(document.createElement("button"))
+            const article = document.createElement( 'article' );
+            const section = document.createElement( 'section' );
+            const img = document.createElement( 'img' );
+            const plocation = document.createElement( 'p' );
+            const ptagline = document.createElement( 'p' );
+            const pprice = document.createElement( 'p' );
+            const a = document.createElement( 'a' )
+            const headercriteres = document.getElementById("headercriteres")
+            
+            img.setAttribute("src", picture)
+            img.setAttribute("alt", "Portrait de " + name)
+            plocation.textContent = city + ", " + country;
+            ptagline.textContent = tagline;
+            pprice.textContent = price + "€/jour";
+            
+            plocation.classList.add("location")
+            ptagline.classList.add("tagline")
+            pprice.classList.add("price")
+            
+            if(page == "home"){
+                const h2 = document.createElement( 'h2' );
+                h2.textContent = name;
+                article.appendChild(h2);
+                article.appendChild(a);
+                a.setAttribute("href", "./photographer.html?id="+ id);
+                a.appendChild(img);
+            }else{
+                const h1 = document.createElement( 'h1' );
+                h1.textContent = name;
+                article.appendChild(h1);
+            }
+            
+            article.appendChild(plocation);
+            article.appendChild(ptagline);
+            
+            if(page == "home"){
+                article.appendChild(pprice);
+                section.classList.add("categories")
+                article.appendChild(section);
+                hashtags()
+            }
+            
+            //Ajoute les hashtags et leurs utilitées
+            function hashtags(){
+                for(let x = 0; x != categories.length; x++){
+                    let button = section.appendChild(document.createElement("button"))
                 button.innerHTML = categories[x]
                 button.classList.add("categorie")
-                
-                button.addEventListener('click', function(){
-                    function applyCriteres(){
-                        photographers.forEach(element => {
-                            let isInCriteres = false
-                            for(let i in criteres){
-                                if(element.categories.indexOf(criteres[i]) > -1 == false){
-                                    isInCriteres = true
-                                }
-                            }
-                            if(isInCriteres == false){
-                                document.querySelectorAll(".photographer_section article")[photographers.indexOf(element)].style.display = "block"
-                            }else{
-                                document.querySelectorAll(".photographer_section article")[photographers.indexOf(element)].style.display = "none"
-                            }
-                            if(criteres.length == 0){
-                                document.querySelectorAll(".photographer_section article")[photographers.indexOf(element)].style.display = "block"
-                            }
-                        });}
-
-                    if(button.classList.contains("activatetag")){
-                        button.classList.remove("activatetag")
-                        history.replaceState({}, "", window.location.href.replace("/" + button.innerHTML, ""))
-                        document.querySelectorAll(".categories button").forEach(element => {
-                            if(element.innerHTML == button.innerHTML){
-                                element.classList.remove("activatetag")
-                            }
-                        });
-                        criteres.splice(criteres.indexOf(button.innerHTML), 1)
-                        applyCriteres()
-                        console.log(criteres)
-                    }else{
-                        document.querySelectorAll(".categories button").forEach(element => {
-                            if(element.innerHTML == button.innerHTML){
-                                element.classList.add("activatetag")
-                            }
-                        });
-                        if(window.location.href.indexOf("?") > -1 == false){
-                            history.pushState({}, "", window.location + "?/" + button.innerHTML)
-                        }else{
-                            history.pushState({}, "", window.location + "/" + button.innerHTML)
-                        }
-                        criteres.push(button.innerHTML)
-                        applyCriteres()
-                        console.log(criteres)
-
+                buttonEvent(button)
+                }
+                if(headtaged == false){
+                    for(let x in alltags){
+                        let button = headercriteres.appendChild(document.createElement("button"))
+                        button.innerHTML = alltags[x]
+                        button.classList.add("categorie")
+                        buttonEvent(button)
                     }
-                })
+                    headtaged = true
+                }
             }
+            return (article);
         }
-
-        return (article);
+        
+        return { name, picture, price, tagline, city, country, getUserCardDOM }
+    }
+    function applyCriteres(){
+        photographers.forEach(element => {
+            let isInCriteres = false
+            for(let i in criteres){
+                if(element.categories.indexOf(criteres[i]) > -1 == false){
+                    isInCriteres = true
+                }
+            }
+            if(isInCriteres == false){
+                document.querySelectorAll(".photographer_section article")[photographers.indexOf(element)].style.display = "block"
+            }else{
+                document.querySelectorAll(".photographer_section article")[photographers.indexOf(element)].style.display = "none"
+            }
+            if(criteres.length == 0){
+                document.querySelectorAll(".photographer_section article")[photographers.indexOf(element)].style.display = "block"
+            }
+        });
+    }
+    function buttonEvent(button){
+        button.addEventListener('click', function(){
+                        
+            if(button.classList.contains("activatetag")){
+                button.classList.remove("activatetag")
+                history.replaceState({}, "", window.location.href.replace("/" + button.innerHTML, ""))
+                document.querySelectorAll(".categories button, #headercriteres button").forEach(element => {
+                    if(element.innerHTML == button.innerHTML){
+                        element.classList.remove("activatetag")
+                    }
+                });
+                criteres.splice(criteres.indexOf(button.innerHTML), 1)
+                applyCriteres()
+            }else{
+                criteres.push(button.innerHTML)
+                applyCriteres()
+                document.querySelectorAll(".categories button, #headercriteres button").forEach(element => {
+                    if(element.innerHTML == button.innerHTML){
+                        element.classList.add("activatetag")
+                    }
+                });
+            }
+        })
     }
 
-    return { name, picture, price, tagline, city, country, getUserCardDOM }
-}
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,21 +1,94 @@
 let urlParameters = new URLSearchParams(window.location.search)
 let id = urlParameters.get("id")
+const photographersSection = document.querySelector(".photographer_section");
+const photographerHeader = document.querySelector(".photograph-header")
+const filterlist = document.querySelector(".filtres ul")
+const filterrow = document.querySelector(".filtres i")
+const filter = document.querySelectorAll(".filtres li")
+const firstfilter = document.querySelector(".filtres li:nth-child(1)")
+const secondfilter = document.querySelector(".filtres li:nth-child(2)")
+const thirdfilter = document.querySelector(".filtres li:nth-child(3)")
+const photographies = document.querySelector(".photographies")
+const fixedinfos = document.querySelector(".fixedinfos")
+let likes = []
+let titles = []
+let dates = []
+let totallikes = 0
+
 
 async function displayPhotographer(photographers) {
-    const photographersSection = document.querySelector(".photographer_section");
-    const photographerHeader = document.querySelector(".photograph-header")
-
+    
     photographers.forEach((photographer) => {
         if(photographer.id == id){
-        const photographerModel = photographerFactory(photographer);
-        const userSelfDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userSelfDOM);
+            const photographerModel = photographerFactory(photographer);
+            const userSelfDOM = photographerModel.getUserCardDOM("photographer");
+            photographersSection.appendChild(userSelfDOM);
+            
+            const photographerImg = photographerHeader.appendChild(document.createElement("img"))
+            photographerImg.setAttribute("src", "/assets/photographers/" + photographer.portrait)
+            photographerImg.setAttribute("alt", "Portrait de " + photographer.name)
+            let fixedlikes = document.createElement("p")
+            data.media.forEach((media) => {
+                if(media.photographerId == photographer.id){
+                    let publication = document.createElement("article")
+                    let underpublication = document.createElement("div")
+                    let title = document.createElement("h3")
+                    let hearts = document.createElement("p")
+                    if(media.video){
+                        let video = document.createElement("video")
+                        video.src = "/assets/images/photos/" + media.video
+                        publication.appendChild(video)
+                    }else{
+                        let photo = document.createElement("img")
+                        photo.src = "/assets/images/photos/" + media.image
+                        publication.appendChild(photo)
+                    }
+                    totallikes += media.likes
+                    title.innerHTML = media.title
+                    hearts.innerHTML = media.likes + '<i class="fas fa-heart"></i>'
+                    hearts.photoid = media.id
+                    hearts.classList.add("publicationhearts")
+                    publication.date = media.date
+                    likes.push(media.likes)
+                    titles.push(media.title)
+                    dates.push(media.date)
+                    underpublication.appendChild(title)
+                    underpublication.appendChild(hearts)
+                    publication.appendChild(underpublication)
+                    photographies.appendChild(publication)
+                    document.querySelectorAll(".publicationhearts").forEach(element => {
+                        if(media.id == element.photoid){
+                            element.likes = media.likes
+                        }
+                    })
+                }
+            })
+            let fixedprice = document.createElement("p")
+            fixedlikes.innerHTML = totallikes + '<i class="fas fa-heart"></i>'
+            fixedlikes.classList.add("fixedlikes")
+            fixedprice.innerHTML = photographer.price + "€ /jour"
+            fixedinfos.appendChild(fixedlikes)
+            fixedinfos.appendChild(fixedprice)
 
-        const photographerImg = photographerHeader.appendChild(document.createElement("img"))
-        photographerImg.setAttribute("src", "/assets/photographers/" + photographer.portrait)
-        photographerImg.setAttribute("alt", "Portrait de " + photographer.name)
-    }
+            document.querySelectorAll(".photographies article").forEach(article => {
+                tri(likes, parseInt(article.querySelector("p").innerHTML.replace('<i class="fas fa-heart"></i>', "")), article)
+            });
+
+            document.querySelectorAll(".publicationhearts").forEach(element => {
+                element.addEventListener("click", () => {
+                    if(element.likes == element.innerText){
+                        element.innerHTML = parseInt(element.innerHTML) + 1 + '<i class="fas fa-heart" aria-hidden="true"></i>'
+                        totallikes += 1
+                    }else{
+                        element.innerHTML = parseInt(element.innerHTML) - 1 + '<i class="fas fa-heart" aria-hidden="true"></i>'
+                        totallikes -= 1
+                    }
+                    document.querySelector(".fixedlikes").innerHTML = totallikes + '<i class="fas fa-heart" aria-hidden="true"></i>'
+                })
+            })
+        }
     });
+
 };
 
 async function init() {
@@ -23,3 +96,87 @@ async function init() {
     const { photographers } = await getPhotographers();
     displayPhotographer(photographers);
 };
+
+filterlist.addEventListener("click", function(){
+    if(filterlist.opened == true){
+        showHideFilterList("", "", "", "", "", false)
+        setTimeout(() => {
+            filterlist.style.transition = ""
+            secondfilter.style.transition = ""
+            thirdfilter.style.transition = ""
+        }, 400);
+    }else{
+        if(firstfilter.innerHTML.includes("Date")){
+            secondfilter.innerHTML = "Popularité"
+            thirdfilter.innerHTML = "Titre"
+        }
+        if(firstfilter.innerHTML.includes("Popularité")){
+            secondfilter.innerHTML = "Date"
+            thirdfilter.innerHTML = "Titre"
+        }
+        if(firstfilter.innerHTML.includes("Titre")){
+            secondfilter.innerHTML = "Popularité"
+            thirdfilter.innerHTML = "Date"
+        }
+        filterlist.style.transition = "height .4s"
+        secondfilter.style.transition = "transform .4s"
+            thirdfilter.style.transition = "transform .4s"
+        setTimeout(showHideFilterList("rotate(0deg)", ".05em solid white", "translate(0, 0)", "translate(0, 0)", "10em", true), 0);
+    }
+})
+
+filter.forEach((element) =>{
+    element.addEventListener("click", function(e){
+        if(filterlist.opened == true){
+            e.stopPropagation()
+            showHideFilterList("", "", "", "", "", false)
+            setTimeout(() => {
+                filterlist.style.transition = ""
+                secondfilter.style.transition = ""
+                thirdfilter.style.transition = ""
+            }, 400);
+            filterlist.opened = false
+        }
+        firstfilter.innerHTML = element.innerHTML
+        if(element.innerHTML == "Titre"){
+            document.querySelectorAll(".photographies article").forEach(article => {
+                tri(titles, article.querySelector("h3").innerHTML, article)
+            });
+        }
+        if(element.innerHTML == "Popularité"){
+            document.querySelectorAll(".photographies article").forEach(article => {       
+                tri(likes, parseInt(article.querySelector("p").innerHTML.replace('<i class="fas fa-heart"></i>', "")), article)
+            });
+        }
+        if(element.innerHTML == "Date"){
+            document.querySelectorAll(".photographies article").forEach(article => {
+                tri(dates, article.date, article)
+            });
+        }
+        })
+})
+
+function showHideFilterList(rowRotation, filterOneBorder, filterTwoTrans, filterThreeTrans, filterListHeight, invert){
+    filterrow.style.transform = rowRotation
+    firstfilter.style.borderBottom = filterOneBorder
+    secondfilter.style.transform = filterTwoTrans
+    thirdfilter.style.transform = filterThreeTrans
+    filterlist.style.height = filterListHeight
+    filterlist.opened = invert
+}
+
+
+function tri(filtre, domcompare, article){
+    likes.sort((a,b) => {
+        return b-a
+    })
+    titles.sort()
+    dates = dates.sort((a,b) => {
+        return new Date(b) - new Date(a)
+    })
+    filtre.forEach(element => {
+        if(element == domcompare){
+            article.style.order = filtre.indexOf(element) + 1
+        }
+    });
+}
