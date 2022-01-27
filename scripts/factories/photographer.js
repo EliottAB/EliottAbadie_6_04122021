58b170e63b590c58fd10ee192506937af686c709
Abadie Eliott <eliott.abadiesouchar@icloud.com>
2022-01-27 18:54:03 +0100

    test for json error (failed last)

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -2,7 +2,7 @@
 let data = []
 let criteres = []
 let headtaged = false
-fetch("/data/photographers.json")
+fetch("photographers.json")
 .then((res) => {
     const promesse = res.json();
     return promesse
